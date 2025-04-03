c94e903cbbb523711af967e490f5fd254e2feaa3
Haruka Hamasaki <harukabangqi@gmail.com>
2025-04-03 23:18:23 +0900

    step8-レビュー3　salesCトランザクション

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Sales;
+use Illuminate\Support\Facades\DB;
 
 class SalesController extends Controller
 {
@@ -14,21 +15,30 @@ class SalesController extends Controller
         $product_id = $request->input('product_id');
         // 減少のための数 quantityが送られてなければ１を入れる
         $quantity = $request->input('quantity',1); 
-        // 商品の検索
-        $product = Product::find($product_id);
-        // 商品が存在しない、または在庫が不足している場合のバリデーションを行う
-        if(!$product){
-            return response()->json(['message' => '商品が見つかりません。', 404]); 
+
+        try{
+            // DBでまとめて実行させる
+            DB::transaction(function () use ($product_id, $quantity) {
+                // 商品の検索　find→findOrFailだとnullの時勝手に404が飛ぶからエラーにならない（メッセは独自のは飛ばない）
+                $product = Product::find($product_id);
+                // 商品が存在しない、または在庫が不足している場合のバリデーションを行う
+                if(!$product){
+                    // 意図的にエラーを出す為にtryの中に記入 catchに飛んで$eにメッセージが入る
+                    throw new \Exception('商品が見つかりません。');
+                }
+                if($product->stock < $quantity){
+                    throw new \Exception('在庫が不足しています。');
+                }
+                // 在庫を減少させる
+                $product -> stock -= $quantity;
+                Sales::decrease($product);
+                // Salesテーブルに商品IDと購入日時を記録する
+                Sales::insertSales($product_id);
+            });
+            // 自販機にレスポンスを返す
+            return response()->json(['message' => '購入しました。'], 200); 
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e -> getMessage()], 400);
         }
-        if($product->stock < $quantity){
-            return response()->json(['message' => '在庫が不足しています。', 422]); 
-        }
-        // 在庫を減少させる
-        $product->stock -= $quantity;
-        Sales::decrease($product);
-        // Salesテーブルに商品IDと購入日時を記録する
-        Sales::insertSales($product_id);
-        // 自販機にレスポンスを返す
-        return response()->json(['message' => '購入しました。'], 200); 
     }
 }
